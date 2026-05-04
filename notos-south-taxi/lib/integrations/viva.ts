524bbf73af80b150f5abd85314a813f281ffedb4
precimartin6 <precimartin6@gmail.com>
2026-05-04 20:28:35 +0300

    Update viva.ts

--- notos-south-taxi/lib/integrations/viva.ts
+++ notos-south-taxi/lib/integrations/viva.ts
@@ -134,6 +134,7 @@ export async function createPaymentOrder(input: CreateOrderInput): Promise<Creat
   orderCode:   data.orderCode,
   checkoutUrl: `https://www.vivapayments.com/web/checkout?ref=${data.orderCode}`,
 };
+}  
 
 export function verifyWebhook(_payload: unknown): boolean {
   return true;
